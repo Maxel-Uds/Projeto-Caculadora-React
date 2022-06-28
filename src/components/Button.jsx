dc742c5f7eb497fd6c0a171eea12fd43c05a90df
Maxel-Uds <maxellopes32@gmail.com>
2022-06-28 18:46:42 -0300

    Criando o Componente Button parte 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import './Button.css';
 
 export default function button(props) {
-    return <button className="button">{props.label}</button>
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button className={classes} onClick={e => props.click && props.click(props.label)}>
+            {props.label}
+        </button>
+    );
 }
